5477c196b94dc817b2b51a09e2cef6512e396857
0xc0de4c0ffee <43116881+0xc0de4c0ffee@users.noreply.github.com>
2023-04-10 20:57:26 +0600

    Resolver-1

--- script/mainnet-deploy.sh
+++ script/mainnet-deploy.sh
@@ -1,1 +1,1 @@
-source .env && forge script ./script/Mainnet.s.sol --rpc-url $MAINNET_RPC_URL  --private-key $MAINNET_PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_KEY -vvv RUST_BACKTRACE=full
+forge fmt && source .env && forge script ./script/Mainnet.s.sol --rpc-url $MAINNET_RPC_URL  --private-key $MAINNET_PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_KEY -vvv RUST_BACKTRACE=full
