08b945ef45f9ede67e32aa970f4ca501f8b0ded4
0xc0de4c0ffee <43116881+0xc0de4c0ffee@users.noreply.github.com>
2023-04-12 18:23:41 +0600

    cleanup & fix logic - almost there

--- src/Gateway.sol
+++ src/Gateway.sol
@@ -16,40 +16,44 @@ abstract contract Gateway {
     ///
     Gate[] public Gateways;
 
+    string[] private TEMP = ["t1", "t2", "t3", "t4", "t5"];
     /**
      * @dev Selects and construct random gateways for CCIP resolution
      * @param _ipns : name to resolve on testnet e.g. alice.eth
-     * @return gateways : ordered list of gateway URLs for HTTP calls
+     * @param _path : full path for records.json
+     * @param k : pseudo random seeding
+     * @return gateways : pseudo random list of gateway URLs for CCIP-Read
+     * Gateway URL e.g. https://gateway.tld/ipns/f<ipns-hash-hex>/.well-known/eth/virgil/<records>.json?t1=0x0123456789
      */
 
-    function randomGateways(string memory _ipns, string memory _path) public view returns (string[] memory gateways) {
+    function randomGateways(bytes memory _ipns, string memory _path, uint256 k)
+        public
+        view
+        returns (string[] memory gateways)
+    {
         uint256 gLen = Gateways.length;
         uint256 len = (gLen / 2) + 1;
-        if (len > 5) len = 5;
+        if (len > 5) len = 5; // max 5? make updatable max value?
         gateways = new string[](len);
-        // pseudo random seeding
-        uint256 k = uint256(keccak256(abi.encodePacked(_ipns, msg.sender, blockhash(block.number - 1))));
+        string memory _suffix = string.concat("f", bytes2HexString(_ipns, 2), "/", _path, ".json?");
         for (uint256 i; i < len;) {
             k = uint256(keccak256(abi.encodePacked(k, msg.sender))) % gLen;
-            // Gateway @ URL e.g. https://example.xyz/eip155:1/alice.eth/{data}
-            gateways[i++] = Gateways[k]._type == 0
-                ? string.concat("https://", _ipns, ".", Gateways[k].domain, "/", _path)
-                : string.concat("https://", Gateways[k].domain, "/ipns/", _ipns, "/", _path);
-            //string.concat("https://", Gateways[k]._domain, "/eip155", ":", chainID, "/", _ipns, "/{data}");
+            gateways[i++] = string.concat("https://", Gateways[k].domain, "/ipns/", _suffix, TEMP[i], "={data}");
         }
     }
 
-    function Base16(bytes memory _ipns) public pure returns (string memory) {
-        bytes memory converted = new bytes(_ipns.length * 2);
-        bytes memory _base = "0123456789abcdef";
-        uint256 len = _ipns.length;
-        for (uint256 i = 0; i < len; i++) {
-            converted[i * 2] = _base[uint8(_ipns[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(_ipns[i]) % _base.length];
+    function bytes2HexString(bytes memory _buffer, uint256 index) public pure returns (string memory) {
+        bytes memory result = new bytes(_buffer.length * 2);
+        bytes memory B16 = "0123456789abcdef";
+        uint256 len = _buffer.length;
+        for (uint256 i = index; i < len; i++) {
+            result[i * 2] = B16[uint8(_buffer[i]) / 16];
+            result[i * 2 + 1] = B16[uint8(_buffer[i]) % 16];
         }
-        return string(abi.encodePacked("f", converted));
+        return string(result);
     }
-    /// @dev : Gateway and Chain Management Functions
+
+    /// @dev : Gateway Management Functions
 
     modifier onlyDev() {
         require(msg.sender == owner, "Only Dev");
@@ -60,13 +64,13 @@ abstract contract Gateway {
     event RemoveGateway(string indexed domain, uint8 indexed _type, address indexed _dev);
     /**
      * @dev Push new gateway to the list
-     * @param domain : new gateway domain
+     * @param _domain : new gateway domain
      * @param _type : type of new gateway
      */
 
-    function addGateway(string calldata domain, uint8 _type) external onlyDev {
-        Gateways.push(Gate(domain, _type));
-        emit AddGateway(domain, _type, msg.sender);
+    function addGateway(string calldata _domain, uint8 _type) external onlyDev {
+        Gateways.push(Gate(_domain, _type));
+        emit AddGateway(_domain, _type, msg.sender);
     }
 
     /**
@@ -76,10 +80,8 @@ abstract contract Gateway {
     function removeGateway(uint256 _index) external onlyDev {
         require(Gateways.length > 1, "Last Gateway");
         Gate memory _g = Gateways[_index];
-        unchecked {
-            if (Gateways.length > _index + 1) {
-                Gateways[_index] = Gateways[Gateways.length - 1];
-            }
+        if (Gateways.length > _index + 1) {
+            Gateways[_index] = Gateways[Gateways.length - 1];
         }
         Gateways.pop();
         emit RemoveGateway(_g.domain, _g._type, msg.sender);
@@ -89,12 +91,11 @@ abstract contract Gateway {
      * @dev Replace gateway for a given controller
      * @param _index : gateway index to replace
      * @param _type : type of gateway
-     * @param domain : new gateway domain
+     * @param _domain : new gateway domain.tld
      */
-    function replaceGateway(uint256 _index, string calldata domain, uint8 _type) external onlyDev {
-        Gate memory _g = Gateways[_index];
-        Gateways[_index] = Gate(domain, _type);
-        emit AddGateway(domain, _type, msg.sender);
-        emit RemoveGateway(_g.domain, _g._type, msg.sender);
+    function replaceGateway(uint256 _index, string calldata _domain, uint8 _type) external onlyDev {
+        emit RemoveGateway(Gateways[_index].domain, Gateways[_index]._type, msg.sender);
+        Gateways[_index] = Gate(_domain, _type);
+        emit AddGateway(_domain, _type, msg.sender);
     }
 }
