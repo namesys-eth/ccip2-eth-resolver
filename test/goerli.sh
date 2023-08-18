7fd2aec512e9083bcadeb7c0d54ffc50e29e7e56
0xc0de4c0ffee <43116881+0xc0de4c0ffee@users.noreply.github.com>
2023-08-18 23:21:55 +0600

    support web2 & web3 gateways

--- test/goerli.sh
+++ test/goerli.sh
@@ -1,1 +1,1 @@
-forge fmt && source .env && forge test --fork-url $GOERLI_RPC_URL -vvv --fork-block-number 8897777 --gas-report --watch
+forge fmt && source .env && forge test --fork-url $GOERLI_RPC_URL -vvv --fork-block-number 8897777 -vvvv --gas-report --watch
